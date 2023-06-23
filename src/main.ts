ee4aba584f5a3f86f54233e1841a5a87fbdb189f
Esteban Alvarado <estalvgs1999@gmail.com>
2023-06-23 12:07:31 -0600

    fix(web-api): :bug: show full path in exception object

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { AllExceptionFilter } from './common/filters';
+import { TimeoutInterceptor } from './common/interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +11,9 @@ async function bootstrap() {
   const port = configService.get('port');
 
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeoutInterceptor());
+
   await app.listen(port);
 }
 bootstrap();
