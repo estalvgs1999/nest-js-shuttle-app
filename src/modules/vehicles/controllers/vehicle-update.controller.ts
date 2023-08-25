3be4997226d0ed10c35de18dbbe9903f6917bf11
Esteban Alvarado <estalvgs1999@gmail.com>
2023-08-24 19:24:17 -0600

    fix(vehicles): :ambulance: Fix update vehicle controller

--- src/modules/vehicles/controllers/vehicle-update.controller.ts
+++ src/modules/vehicles/controllers/vehicle-update.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Param, Patch, Query } from '@nestjs/common';
+import { Body, Controller, Param, Patch } from '@nestjs/common';
 import { UpdateVehicleService } from '../services';
 import { UpdateVehicleDTO } from '../dtos';
 
@@ -7,7 +7,7 @@ export class UpdateVehicleStatusController {
   constructor(private readonly service: UpdateVehicleService) {}
 
   @Patch('/:id/update')
-  create(@Param('id') id: string, @Query() dto: UpdateVehicleDTO) {
+  create(@Param('id') id: string, @Body() dto: UpdateVehicleDTO) {
     return this.service.run(id, dto);
   }
 }
