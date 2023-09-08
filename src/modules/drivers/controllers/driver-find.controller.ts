89f93c5a298f834c7beb0e548539f7dbb1e296f9
Esteban Alvarado Vargas <estalvgs1999@gmail.com>
2023-09-07 22:34:17 -0600

    feat(drivers): :sparkles: get my drivers profile

--- src/modules/drivers/controllers/driver-find.controller.ts
+++ src/modules/drivers/controllers/driver-find.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { DriverFilterDto } from '../dtos';
 import { FindDriverService } from '../services';
+import { GetCurrentUserId } from '@/common/decorators';
 
 @Controller({ path: 'driver' })
 export class FindDriverController {
@@ -9,6 +10,11 @@ export class FindDriverController {
   @Get('/:id')
   findById(@Param('id') driverId: string) {
     return this.service.findById(driverId);
+  }
+
+  @Get('/profile/me')
+  findMyProfile(@GetCurrentUserId() userId: string) {
+    return this.service.findByUser(userId);
   }
 
   @Get()
